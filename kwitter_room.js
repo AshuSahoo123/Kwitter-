f1a26dd7093352cfa677a572e128798432b8f04f
AshuSahoo123 <84637653+AshuSahoo123@users.noreply.github.com>
2022-07-24 13:52:01 -0500

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,14 +1,16 @@
 var firebaseConfig = {
-      apiKey: "AIzaSyDvBXcLia9zQY50EePFgjCC33_jIRKX53s",
-      authDomain: "kwitter-6898d.firebaseapp.com",
-      databaseURL: "https://kwitter-6898d-default-rtdb.firebaseio.com",
-      projectId: "kwitter-6898d",
-      storageBucket: "kwitter-6898d.appspot.com",
-      messagingSenderId: "475519775163",
-      appId: "1:475519775163:web:8b7d203e9d284c9996d49e"
+      apiKey: "AIzaSyDXGzsnPoDPRmRUunf1W2-BQuwGxZxejTw",
+      authDomain: "kwitter-dd81e.firebaseapp.com",
+      databaseURL: "https://kwitter-dd81e-default-rtdb.firebaseio.com",
+      projectId: "kwitter-dd81e",
+      storageBucket: "kwitter-dd81e.appspot.com",
+      messagingSenderId: "46026378519",
+      appId: "1:46026378519:web:692ff46f8db548101cfb52"
     };
     
-    firebase.initializeApp(firebaseConfig);
+    // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+    
 
     user_name = localStorage.getItem("user_name");
     document.getElementById("user_name").innerHTML = "Welcome " + user_name + " !";
@@ -26,13 +28,13 @@ function getData() {
       firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-            row = "<div class = 'room_name' id = " + Room_names + "onclick = 'redirect1(this.id)'> #" + Room_names + "</div> <hr>";
+            row = "<div class = 'room_name' id = " + Room_names + "onclick = 'redirecttoroom(this.id)'> #" + Room_names + "</div> <hr>";
             document.getElementById("output").innerHTML += row;
       //End code
       });});}
 getData();
 
-function redirect1(name) {
+function redirecttoroom(name) {
       console.log(name);
       localStorage.setItem("room_name", name);
       window.location = "kwitter_page.html";
